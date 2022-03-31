f017924620d538f769022aaf88de46c07ecae85d
nova <adfzmxx@163.com>
2022-03-31 20:05:17 +0800

    init

---
+++ src/app.config.ts
@@ -0,0 +1,14 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index'
+  ],
+  components: [
+    'components/apple/apple'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'WeChat',
+    navigationBarTextStyle: 'black'
+  }
+})
---
+++ src/components/apple/apple.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import { View, Text } from '@tarojs/components'
+
+export default class Apple extends React.Component {
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello Apple!</Text>
+      </View>
+    )
+  }
+}
